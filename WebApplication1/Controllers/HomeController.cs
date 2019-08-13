5cd1bd24a1e451c4ad1857dfa6039c5b750031d5
Neil Tao Kuei-Liang <neil.tao83@titansoft.com.sg>
2019-08-13 17:06:31 +0800

    add language

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace WebApplication1.Controllers
 
 		public ActionResult About()
 		{
+			var a = "this is language";
+
 			ViewBag.Message = "Your application description page.";
 
 			return View();
